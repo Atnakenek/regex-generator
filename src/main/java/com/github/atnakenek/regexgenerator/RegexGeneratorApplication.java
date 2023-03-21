38fae3dd1728f079e0fb3cf0e14d7894705dda52
fabio.rende <fabio.rende@rgigroup.com>
2023-03-21 15:03:53 +0100

    initial commit

---
+++ src/main/java/com/github/atnakenek/regexgenerator/RegexGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.github.atnakenek.regexgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegexGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegexGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/atnakenek/regexgenerator/RegexGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.atnakenek.regexgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegexGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
